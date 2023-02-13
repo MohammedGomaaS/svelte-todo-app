e8f5e1286af94be4fdc7cc5302b88a9367ef70fb
Huda Farag <huda.farag@dxwand.com>
2023-02-13 10:59:38 +0200

    folder item component initialized

---
+++ src/FolderItem.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  export let folder;
+  const dispatch = createEventDispatcher();
+  function setActiveFolder() {
+    dispatch("setActiveFolder", {
+      folder,
+    });
+  }
+</script>
+
+<div class="todo-item" on:click={setActiveFolder}>
+  {folder.name}
+</div>
--- src/Folders.svelte
+++ src/Folders.svelte
@@ -1,50 +1,51 @@
 <script>
-    import Todo from "./Todo.svelte";
-    let activeFolder;
-    let newFolderName = "";
-    let folders = [
-      {
-        id: 1,
-        name: "folder1",
+  import Todo from "./Todo.svelte";
+  import FolderItem from "./FolderItem.svelte";
+  let activeFolder;
+  let newFolderName = "";
+  let folders = [
+    {
+      id: 1,
+      name: "folder1",
+      todoList: [],
+    },
+  ];
+  function handleSetActiveFolder(event) {
+    activeFolder = event.detail.folder;
+  }
+  function addNewFolder() {
+    if (event.key === "Enter") {
+      folders.push({
+        name: newFolderName,
+        id: Math.max(...folders.map((folder) => folder.id)) + 1,
         todoList: [],
-      }
-    ];
-    function setActiveFolder(folder) {
-      activeFolder = folder;
+      });
+      folders = folders;
     }
-    function addNewFolder() {
-      if (event.key === "Enter") {
-        folders.push({
-          name: newFolderName,
-          id: Math.max(...folders.map(folder => folder.id)) + 1,
-          todoList: [],
-        });
-        folders = folders;
-      }
-    }
-    function handleUpdateActiveFolderTodoList(event) {
-      folders.find(folder => folder.id === activeFolder.id).todoList = event.detail.todoList
-    }
-  </script>
-  <div class="folders">
-    <input
-      type="text"
-      placeholder="New folder ..."
-      bind:value={newFolderName}
-      on:keydown={addNewFolder}
+  }
+  function handleUpdateActiveFolderTodoList(event) {
+    folders.find((folder) => folder.id === activeFolder.id).todoList =
+      event.detail.todoList;
+  }
+</script>
+
+<div class="folders">
+  <input
+    type="text"
+    placeholder="New folder ..."
+    bind:value={newFolderName}
+    on:keydown={addNewFolder}
+  />
+  {#each folders as folder}
+    <FolderItem {folder} on:setActiveFolder={handleSetActiveFolder} />
+  {/each}
+</div>
+<div class="folder-todolist">
+  {#if activeFolder}
+    <Todo
+      folderName={activeFolder.name}
+      todoList={activeFolder.todoList}
+      on:updateActiveFolderTodoList={handleUpdateActiveFolderTodoList}
     />
-    {#each folders as folder}
-      <div class="todo-item" on:click={() => setActiveFolder(folder)}>
-        {folder.name}
-      </div>
-    {/each}
-  </div>
-  <div class="folder-todolist">
-    {#if activeFolder}
-      <Todo
-        folderName={activeFolder.name}
-        todoList={activeFolder.todoList}
-        on:updateActiveFolderTodoList={handleUpdateActiveFolderTodoList}
-      />
-    {/if}
-  </div>
+  {/if}
+</div>
